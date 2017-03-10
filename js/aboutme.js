f5e674873be79ac9fe0ad8372bd1ca9119c82c74
MaetDol <rndka9@gmail.com>
2017-03-10 13:11:56 -0500

    AboutMe 페이지 본문 추가, Github링크 생성. Email링크는 아직임

--- js/aboutme.js
+++ js/aboutme.js
@@ -1,3 +1,19 @@
 $(document).ready (function () {
   $("#Card").addClass ('turnOver');
+
+  $("#Email .svg").hover (function () {
+    $(this).siblings (".description").stop ().fadeTo (200, 1);
+    $(this).siblings (".arrow").stop ().fadeTo (200, 1);
+  }, function () {
+    $(this).siblings (".description").stop ().fadeTo (100, 0);
+    $(this).siblings (".arrow").stop ().fadeTo (100, 0);
+  });
+
+  $("#Github .svg").hover (function () {
+    $(this).parents ().siblings (".description").stop ().fadeTo (200, 1);
+    $(this).parents ().siblings (".arrow").stop ().fadeTo (200, 1);
+  }, function () {
+    $(this).parents ().siblings (".description").stop ().fadeTo (100, 0);
+    $(this).parents ().siblings (".arrow").stop ().fadeTo (100, 0);
+  });
 });
