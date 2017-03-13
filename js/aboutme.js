73af4061317d44b2e6a36ef8a0814016fd4b3593
MaetDol <rndka9@gmail.com>
2017-03-13 09:02:30 -0400

    AboutMe에서 이메일버튼 클릭시 이메일 복사, 복사 후 복사 완료를 알려줌. 깃허브와 이메일 호버시 동작방식(js)을 수정함

--- js/aboutme.js
+++ js/aboutme.js
@@ -1,19 +1,47 @@
+function appear (ele) {
+  ele.stop ().show ().fadeTo (200, 1);
+}
+
+function disappear (ele) {
+  ele.stop ().fadeTo (150, 0).hide();
+}
+
 $(document).ready (function () {
   $("#Card").addClass ('turnOver');
 
   $("#Email .svg").hover (function () {
-    $(this).siblings (".description").stop ().fadeTo (200, 1);
-    $(this).siblings (".arrow").stop ().fadeTo (200, 1);
+    appear ($(this).siblings (".description"));
+    appear ($(this).siblings (".arrow"));
   }, function () {
-    $(this).siblings (".description").stop ().fadeTo (100, 0);
-    $(this).siblings (".arrow").stop ().fadeTo (100, 0);
+    disappear ($(this).siblings (".description"));
+    disappear ($(this).siblings (".arrow"));
   });
 
   $("#Github .svg").hover (function () {
-    $(this).parents ().siblings (".description").stop ().fadeTo (200, 1);
-    $(this).parents ().siblings (".arrow").stop ().fadeTo (200, 1);
+    appear ($(this).parents ().siblings (".description"));
+    appear ($(this).parents ().siblings (".arrow"));
   }, function () {
-    $(this).parents ().siblings (".description").stop ().fadeTo (100, 0);
-    $(this).parents ().siblings (".arrow").stop ().fadeTo (100, 0);
+    disappear ($(this).parents ().siblings (".description"));
+    disappear ($(this).parents ().siblings (".arrow"));
   });
+
+  $('#Email .svg').on ('click', function (e) {
+    $('.copied').remove ();
+
+    var content = $(this).next ();
+    var range = document.createRange ();
+    range.selectNode (content[0]);
+    window.getSelection ().addRange (range);
+
+    var successful = document.execCommand ('copy');
+
+    $('.description.clickToCopy').after (
+      '<span class="copied">복사완료!</span>',
+      '<div  class="copied"></div>');
+    $('.copied').fadeTo (200, 1).delay (1000).fadeTo (200, 0).queue (
+      function () { $(this).remove(); });
+
+    window.getSelection ().removeAllRanges ();
+  });
+
 });
