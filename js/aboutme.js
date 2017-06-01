2601e7d1e68b039ba384b51f99abaa52b133c13e
MaetDol <rndka9@gmail.com>
2017-06-01 19:14:15 +0900

    add Category title, content, js, css little bit

--- js/aboutme.js
+++ js/aboutme.js
@@ -10,32 +10,32 @@ $(document).ready (function () {
   $("#Card").addClass ('turnOver');
 
   $("#Email .svg").hover (function () {
-    appear ($(this).siblings (".description"));
-    appear ($(this).siblings (".arrow"));
+    appear ($(this).siblings (".tooltipBubble"));
+    appear ($(this).siblings (".tooltipArrow"));
   }, function () {
-    disappear ($(this).siblings (".description"));
-    disappear ($(this).siblings (".arrow"));
+    disappear ($(this).siblings (".tooltipBubble"));
+    disappear ($(this).siblings (".tooltipArrow"));
   });
 
   $("#Github .svg").hover (function () {
-    appear ($(this).parents ().siblings (".description"));
-    appear ($(this).parents ().siblings (".arrow"));
+    appear ($(this).parents ().siblings (".tooltipBubble"));
+    appear ($(this).parents ().siblings (".tooltipArrow"));
   }, function () {
-    disappear ($(this).parents ().siblings (".description"));
-    disappear ($(this).parents ().siblings (".arrow"));
+    disappear ($(this).parents ().siblings (".tooltipBubble"));
+    disappear ($(this).parents ().siblings (".tooltipArrow"));
   });
 
   $('#Email .svg').on ('click', function (e) {
     $('.copied').remove ();
 
     var content = $(this).next ();
-    var range = document.createRange ();
+    var range   = document.createRange ();
     range.selectNode (content[0]);
     window.getSelection ().addRange (range);
 
     var successful = document.execCommand ('copy');
 
-    $('.description.clickToCopy').after (
+    $('.tooltipBubble.copyEmail').after (
       '<span class="copied">복사완료!</span>',
       '<div  class="copied"></div>');
     $('.copied').fadeTo (200, 1).delay (1000).fadeTo (200, 0).queue (
