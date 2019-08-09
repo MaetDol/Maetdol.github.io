aeec089f83004dd0acd637b222fc41b8364c0efb
maetdol <rndka9@gmail.com>
2019-08-09 11:37:16 +0900

    Improved detecting mobile
    Code from Stackoverflow
    link: https://stackoverflow.com/questions/11381673/detecting-a-mobile-browser

--- js/index.js
+++ js/index.js
@@ -1,15 +1,12 @@
 // 파이어 폭스에서 뒤로가기시, JS가 작동하지 않는 문제를 해결하기 위해 사용
-window.onunload = function () {};
-
 window.onpageshow = function (e) {
 
-  if (e.persisted)
-    $('#Card').removeClass('turnOver');
+  if (e.persisted) $('#Card').removeClass('turnOver');
 
   $('#Card').removeClass('to-right');
 
-  $("#AboutMe").on ("click", function () {
-    $("#Card").addClass ('turnOver').delay (300).queue (function () {
+  $("#AboutMe").on("click", function () {
+    $("#Card").addClass('turnOver').delay(300).queue(function () {
       location.href = 'AboutMe.html';
     });
   });
